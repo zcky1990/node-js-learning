56f43f7af22d1c7cabed00b7bd2afff3825b665e
soeltan zaki rizaldy <zcky1990@gmail.com>
2024-02-10 02:36:46 +0700

    fix test sample

--- app/controllers/api/test.js
+++ app/controllers/api/test.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Wiki home page");
+    res.send("Wiki home page");
 });
 
 router.get("/about", function (req, res) {
-  res.send("About this wiki");
+    res.send("About this wiki");
 });
 
 module.exports = router;
