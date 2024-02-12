a26ce5128f9d410c9318e956ab3c8dc86f84206e
soeltan zaki rizaldy <zcky1990@gmail.com>
2024-02-12 19:11:46 +0700

    add quote components

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -73,7 +73,7 @@ router.get("/", function (req, res) {
 				}
 			],
 			envelop: {
-				// backgroundImage: 'https://websitedemos.net/fiona-patisserie-04/wp-content/uploads/sites/1136/2022/11/bg-03.jpg',
+				backgroundImage: 'https://websitedemos.net/fiona-patisserie-04/wp-content/uploads/sites/1136/2022/11/bg-03.jpg',
 				backgroundColor: '#f9f3ed'
 			},
 			gallery: [
